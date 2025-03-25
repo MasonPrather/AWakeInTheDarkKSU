d73fcca070914774fadb2625cf323e62f18961d1
gingerwright <111036227+gingerwright@users.noreply.github.com>
2025-03-25 00:27:37 -0400

    Change cursor style when hovering over clickable areas

--- awakeinthedark/Scripts/key_area.gd
+++ awakeinthedark/Scripts/key_area.gd
@@ -5,9 +5,17 @@ var item_name = "key"
 func _ready():
 	input_pickable = true
 	connect("input_event", Callable(self, "_on_input_event"))
+	connect("mouse_entered", Callable(self, "_on_mouse_entered"))
+	connect("mouse_exited", Callable(self, "_on_mouse_exited"))
 
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		print(item_name + " was clicked!")
 		# Call your interaction function here
 		get_parent().handle_item_interaction(item_name)
+		
+func _on_mouse_entered():
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+	
+func _on_mouse_exited():
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
